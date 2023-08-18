9b6afc1e27c4992c1e39b9260a8d1d3b8352b7f4
LineWulff <lw@himalaya.dk>
2023-08-18 11:30:16 -0600

    Adding two main analysis scripts (CD45 + CD4 based)

---
+++ 2308_CD4pos.R
@@ -0,0 +1,11 @@
+#' R script for producing outputs in from **CD4+ cells** from Marcela's project
+#' Author: Line Wulff
+#' Date (created): 23-08-18
+
+#### ---- Initiate libraries ---- ####
+
+
+#### ---- Read in data, inspect and transform if necessary ---- ####
+
+
+
